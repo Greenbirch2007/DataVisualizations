d29a488e888ba78e6e8815024a9c5596698ecf3c
Karson <34601389+Greenbirch2007@users.noreply.github.com>
2019-02-13 20:34:22 +0800

    精确定位进场时点和进场成本(便于反思)
    精确定位进场时点和进场成本(便于反思)

--- 实践之路/A股接口编写/static/astock.js
+++ 实践之路/A股接口编写/static/astock.js
@@ -1,4 +1,4 @@
-
+// 2019.2.13
 
 
 // getJSON(url,作用的返回内容)  # 接口单项请求请求成功
@@ -11,8 +11,17 @@ $.getJSON('http://127.0.0.1:5000/astock',function (content) {
            text: '可视化测试'
        },
        subtitle: {
-           text: '进场成本-21.3'
+           text: '进场时点和成本(4800,-21.3)'
        },
+         xAxis: {
+             // ... 省略代码
+             plotLines:[{
+                 color:'green',            //线的颜色，定义为红色
+                 dashStyle:'longdashdot',//标示线的样式，默认是solid（实线），这里定义为长虚线
+                 value:4800,                //定义在哪个值上显示标示线，这里是在x轴上刻度为3的值处垂直化一条线
+                 width:2                 //标示线的宽度，2px
+             }]
+         },
        yAxis: {
            opposite:true,
            title: {
@@ -22,7 +31,7 @@ $.getJSON('http://127.0.0.1:5000/astock',function (content) {
                color:'red',           //线的颜色，定义为红色
                dashStyle:'Dash',     //默认值，这里定义为实线
                value:-21.3,               //定义在那个值上显示标示线，这里是在x轴上刻度为3的值处垂直化一条线
-               width:2                //标示线的宽度，2px
+               width:1.6                //标示线的宽度，2px
            }]
 
        },
